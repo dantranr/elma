4b7d6026385f579e73e081de70a82f0a4dafc651
Dan Tran <dan.gbu26@gmail.com>
2019-03-26 11:00:20 -0700

    update the main.cc with test cases

--- src/main.cc
+++ src/main.cc
@@ -1,25 +1,35 @@
 #include <iostream>
-#include <chrono>
-#include <vector>
-#include <ncurses.h>
 
-#include "stopwatch.h"
+#include "elmacar.h"
 
-using namespace std::chrono;
 using namespace elma;
-using namespace stopwatch;
+using namespace elmacar;
 
 int main() {
+    
+    ElmaCar *ElmaCar = new Elmacar();
+	
+	ElmaCar->ElmaCarEngine->Start();
+	std::cout << "Elma Car Engine Started!" << std::endl;
+	
+	ElmaCar->ElmaCarWheels->Accelebrate(20);
+	std::cout << "The car is going: " << ElmaCar->ElmaCarWheels->GetSpeed() << std::endl;
+	
+    ElmaCar->ElmaCarWheels->Decelebrate(10);
+	std::cout << "The car is going: " << ElmaCar->ElmaCarWheels->GetSpeed() << std::endl;
+	
+	ElmaCar->ElmaCarBrakes->Apply(10);
+	std::cout << "Apply the brakes of 10." << std::endl;
+	std::cout << "The car is going: " << ElmaCar ->ElmaCarWheels->GetSpeed() << std::endl;
+	
+    ElmaCar->ElmaCarWheels->Accelebrate(1000);
+	std::cout << "The car is going: " << ElmaCar->ElmaCarWheels->GetSpeed() << std::endl;
+	
+	ElmaCar->ElmaCarBrakes->Apply(100);
+	std::cout << "Apply the brakes of 100." << std::endl;
+	std::cout << "The car is going: " << ElmaCar ->ElmaCarWheels->GetSpeed() << std::endl;
+    
+	ElmaCar->ElmaCarEngine->Stop();
+	std::cout << "Elma Car Engine Stopped!" << std::endl;
 
-    Manager m;
-    StopWatch stopwatch;
-    UserInterface ui(stopwatch);
-
-    m.schedule(ui, 10_ms)
-     .schedule(stopwatch, 10_ms)     
-     .init()
-     .run();
-   
-    endwin();     
-     
 }
